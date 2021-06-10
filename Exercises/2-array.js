2f061eebe6fc3f37269581cef153db91043b87bf
Nikolai <nlv.nki@gmail.com>
2021-06-10 17:34:08 +0300

    refactoring ex. code

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,34 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const result = [];
+  function wrapper(value) {
+    let typeValue = typeof value;
+    if (typeValue !== 'number') {
+     let error = new Error('Only numbers are allowed')
+      return error;
+    }
+    return result[value];
+  }
+  wrapper.push = (value) => {
+    result.push(value);
+  }
+  wrapper.pop = (value) => {
+    result.pop(value);
+  }
+  return wrapper;
+};
 
 module.exports = { array };
+
+
+
+const arr = array();
+
+arr.push('first');
+arr.push('second');
+arr.push('third');
+console.log(arr(0)); // Выводит: first
+console.log(arr(1)); // Выводит: second
+console.log(arr(2)); // Выводит: third
+console.log(arr('test')); // Выводит: ошибку
